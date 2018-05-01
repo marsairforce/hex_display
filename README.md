ebca6913eb3e6cd69f60e154dc5268a21c46824b
Travis Hein <marsairforce@gmail.com>
2018-05-01 01:52:25 -0400

    first attempt at firmware.
    now that i am able to actually program some software onto this thing.

--- README.md
+++ README.md
@@ -5,3 +5,26 @@ Wanting to have some convenient module for displaying address bus contents.
 This is a 28 DIP form factor circuit board using an Atmega 164A. Because it has a lot of IO pins.
 
 Inspired by https://www.youtube.com/watch?v=o9eQ9NhFT60
+
+# V1 Hardware
+Using Atmega164A device and 4 10mm 7 segment displays.
+
+![fig1](photos/hex_display1.png)
+![fig2](photos/hex_display2.png)
+
+The device was chosen because it has enough IO pins to directly drive all the segments with display multiplexing (the common anodes, using a transistor driver).
+
+It was supposed to be a 28 pin dip format, but it is accidentally almost too wide to fit on a breadboard. Well, almost too wide. some cretive wiring under the display module on the breadboard should work.
+
+Since we have pins for the MISO, MOSI, SCK. We can have a mode where this thing is an I2C or SPI slave and displays values on an I2C bus? That might be an intersting use. A display module. Though I would probably just use it as a clock since thats all I seem to build these days.
+
+# Programming
+At first I had some learning curve to program this. I was using the Arduino IDE (instead of spending time to get avr-gcc and the tool chain to work on its own with a Makefile, becuase.. macbook here..).
+
+The AT164A is not directly supported hardware, but there is this project for [MightyCore](https://github.com/MCUdude/MightyCore) that seems to bring the AT164 support among others to the Arduino IDE.
+
+I tried to use the "Arduino as ISP" sketch to create an ICSP and attached it to the pings on my module. Though it would not be recognized still.  I verified the Arduino as ISP setup worked by programming an Attiny85. That led to a tangent project to learn how to build a HVSP programmer on a second arduino to rese the fuzes, because I bricked the Attiny85 by accident. And then the desire to create an all in one programmer that can handle both cases. But that fell apart when I discovered there is already a usbtiny programmer project and not wanting to lose focus on the bigger picture which was to get this damn device I built in like February (it is now late April) to program so I can just see if it even works. You know. prototyping by designing and fabricating a PCB from the get go because stupid surface mount everything, what can possibly go wrong. Oh right, everything.
+
+So then putting some energy intiwanting to build the usbtinyisp, as maybe that would work for me instead of the Arduino as ISP thing, but feeling no budget lately, and then remembering I had a bunch of digispark devices that have the same-ish kind of hardware that the usbtiny programmer has. So then the idea to use this as a usbtiny isp. And a few iterations with the arduino as isp and HVSP programmers to realize the USB cable I was using for it was really a 2 wire power cable and not even a USB cable at all. How many sleepless nights are we up to now anyway. At least we now have a fork on Github for the vusbtiny project, with digispark hardware.
+
+So getting the usbtiny programmer to work, and then learning we need to edit the avrdude.conf file to allow it to recognize the Atmega164A device. And then learning the Arduino IDE supports a `Upload using programmer` mode, so then going back to just using that. And here we are. Other than some random crazy ramblings and a somewhat arbitrary path to get here, I have uninteligently managed to use Arduino IDE with MightyCore, the updated avrdude.conf, with a usbtinyisp.
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -6,52 +6,52 @@
  * I used https://camo.githubusercontent.com/46718caeab6022a330ffed7f27109c147a16ba56/68747470733a2f2f692e696d6775722e636f6d2f735954747555492e6a7067
  * Inputs:
  * | Function | IC | Atmega | Pin | Aruino Pin reference
- * |      /OE |  1 |    PB4 |  44 |   10 |
- * |       A0 |  2 |    PA0 |  37 |   21 |
- * |       A1 |  3 |    PA1 |  36 |   20 |
- * |       A2 |  4 |    PA2 |  35 |   19 |
- * |       A3 |  5 |    PA3 |  34 |   18 |
- * |       A4 |  6 |    PA4 |  33 |   17 |
- * |       A5 |  7 |    PA5 |  32 |   16 |
- * |       A6 |  8 |    PA6 |  31 |   15 |
- * |       A7 |  9 |    PA7 |  30 |   14 |
- * |       A8 | 10 |    PC0 |  19 |   22 |
- * |       A9 | 11 |    PC1 |  20 |   23 |
- * |      A10 | 12 |    PC2 |  21 |   24 |
- * |      A11 | 13 |    PC3 |  22 |   25 |
- * |      A12 | 16 |    PC4 |  23 |   26 |
- * |      A13 | 17 |    PC5 |  24 |   27 |
- * |      A14 | 18 |    PC6 |  25 |   28 |
- * |      A15 | 19 |    PC7 |  26 |   29 |
+ * |      /OE |  1 |    PB4 |  44 |    4 |
+ * |       A0 |  2 |    PA0 |  37 |   24 |
+ * |       A1 |  3 |    PA1 |  36 |   25 |
+ * |       A2 |  4 |    PA2 |  35 |   26 |
+ * |       A3 |  5 |    PA3 |  34 |   27 |
+ * |       A4 |  6 |    PA4 |  33 |   28 |
+ * |       A5 |  7 |    PA5 |  32 |   29 |
+ * |       A6 |  8 |    PA6 |  31 |   30 |
+ * |       A7 |  9 |    PA7 |  30 |   31 |
+ * |       A8 | 10 |    PC0 |  19 |   16 |
+ * |       A9 | 11 |    PC1 |  20 |   17 |
+ * |      A10 | 12 |    PC2 |  21 |   18 |
+ * |      A11 | 13 |    PC3 |  22 |   19 |
+ * |      A12 | 16 |    PC4 |  23 |   20 |
+ * |      A13 | 17 |    PC5 |  24 |   21 |
+ * |      A14 | 18 |    PC6 |  25 |   22 |
+ * |      A15 | 19 |    PC7 |  26 |   23 |
+ * |       LE | 15 |    PD7 |  16 |   15 |
  * |       NC | 20 |        |     |      |
  * |       NC | 21 |        |     |      |
  * |       NC | 22 |        |     |      |
  * |     DVCC | 23 |        |     |      |
  * |    RESET | 24 |  RESET |   4 |      |
- * |      SCK | 25 |    SCK |   3 |   13 |
- * |     MISO | 26 |   MISO |   2 |   12 |
- * |     MOSI | 27 |   MOSI |   1 |   11 |
+ * |      SCK | 25 |    SCK |   3 |    7 |
+ * |     MISO | 26 |   MISO |   2 |    6 |
+ * |     MOSI | 27 |   MOSI |   1 |    5 |
  * |      GND | 14 |    GND | 6,18,28,39 |
  * |      VCC | 28 |    VCC | 5,17,27,38 |
  *
  *
  * Connections to LEDs:
  * | Function | Atmega | Pin | Arduino Pin Reference |
- * |       Ca |    PD0 |   9 |   0 |
- * |       Cb |    PD1 |  10 |   1 |
- * |       Cc |    PD2 |  11 |   2 |
- * |       Cd |    PD3 |  12 |   3 |
- * |       Ce |    PD4 |  13 |  30 |
- * |       Cf |    PD5 |  14 |   8 |
- * |       Cg |    PD6 |  15 |   9 |
+ * |       Ca |    PD0 |   9 |   8 |
+ * |       Cb |    PD1 |  10 |   9 |
+ * |       Cc |    PD2 |  11 |  10 |
+ * |       Cd |    PD3 |  12 |  11 |
+ * |       Ce |    PD4 |  13 |  12 |
+ * |       Cf |    PD5 |  14 |  13 |
+ * |       Cg |    PD6 |  15 |  14 |
  * |
- * |      An0 |    PB3 |  43 |   7 |
- * |      An1 |    PB2 |  42 |   6 |
- * |      An2 |    PB1 |  41 |   5 |
- * |      An3 |    PB0 |  40 |   4 |
+ * |      An0 |    PB3 |  43 |   3 |
+ * |      An1 |    PB2 |  42 |   2 |
+ * |      An2 |    PB1 |  41 |   1 |
+ * |      An3 |    PB0 |  40 |   0 |
 
  * Not connected: 
- * |          |    PD7 |  16 |
  * |          |  XTAL2 |   7 |
  * |          |  XTAL1 |   8 |
  * |          |   AREF |  29 |
@@ -64,37 +64,164 @@
  * |     MOSI |      11 |    4 |      27 |    1 |
  * |     MISO |      12 |    1 |      26 |    2 |
  * |      SCK |      13 |    3 |      25 |    3 |
- * |    RESET |      10 |    - |      24 |    4 |
+ * |    RESET |      10 |    5 |      24 |    4 |
  * |      GND |     GND |    6 |      14 |    6 |
  * 
  
  */
 
-#define PIN_AN0   7
-#define PIN_AN1   6
-#define PIN_AN2   5
-#define PIN_AN3   4
+#define PIN_AN0   3
+#define PIN_AN1   2
+#define PIN_AN2   1
+#define PIN_AN3   0
 
-#define PIN_C_A   0
-#define PIN_C_B   1
-#define PIN_C_C   2
-#define PIN_C_D   3
-#define PIN_C_E  30 
-#define PIN_C_F   8
-#define PIN_C_G   9
+#define PIN_MOSI  5
+#define PIN_MISO  6
+#define  PIN_SCK   7
+
+#define PIN_CA    8
+#define PIN_CB    9
+#define PIN_CC    10
+#define PIN_CD    11
+#define PIN_CE    12 
+#define PIN_CF    13
+#define PIN_CG    14
+
+#define PIN_LE    15
+#define PIN_OE    4
+
+#define PIN_A0    24
+#define PIN_A1    25
+#define PIN_A2    26
+#define PIN_A3    27
+#define PIN_A4    28
+#define PIN_A5    29
+#define PIN_A6    30
+#define PIN_A7    31
+#define PIN_A8    16
+#define PIN_A9    17
+#define PIN_A10   18
+#define PIN_A11   19
+#define PIN_A12   20
+#define PIN_A13   21
+#define PIN_A14   22
+#define PIN_A15   23
+
+
+// bit matrix for active low cathodes to display 0..F
+// where this is really just port D, segment a is LSB, segment g is bit 0x40 (mask is 0x7F)
+// 0  100 0000    0x40
+// 1  111 1001    0x79
+// 2  010 0100    0x24
+// 3  011 0000    0x30
+// 4  001 1001    0x19
+// 5  001 0010    0x12
+// 6  000 0010    0x02
+// 7  111 1000    0x78
+// 8  000 0000    0x00
+// 9  001 1000    0x18
+// A  000 1000    0x08
+// b  000 0011    0x03
+// C  100 0110    0x46
+// d  010 0001    0x21
+// E  000 0110    0x06
+// F  000 1110    0x0E
+
+const char digits[16] = { 0x40, 0x79, 0x24, 0x30, 0x19, 0x12, 0x02, 0x78, 0x00, 0x18, 0x08, 0x03, 0x46, 0x21, 0x06, 0x0E };
 
 void setup() {
   pinMode(PIN_AN0, OUTPUT);
-  pinMode(PIN_C_A, OUTPUT);
+  pinMode(PIN_AN1, OUTPUT);
+  pinMode(PIN_AN2, OUTPUT);
+  pinMode(PIN_AN3, OUTPUT);
+  
+  pinMode(PIN_CA, OUTPUT);
+  pinMode(PIN_CB, OUTPUT);
+  pinMode(PIN_CC, OUTPUT);
+  pinMode(PIN_CD, OUTPUT);
+  pinMode(PIN_CE, OUTPUT);
+  pinMode(PIN_CF, OUTPUT);
+  pinMode(PIN_CG, OUTPUT);
+
+  pinMode(PIN_LE, INPUT);
+  pinMode(PIN_OE, INPUT);
+
+  pinMode(PIN_A0, INPUT_PULLUP);
+  pinMode(PIN_A1, INPUT_PULLUP);
+  pinMode(PIN_A2, INPUT_PULLUP);
+  pinMode(PIN_A3, INPUT_PULLUP);
+  pinMode(PIN_A4, INPUT_PULLUP);
+  pinMode(PIN_A5, INPUT_PULLUP);
+  pinMode(PIN_A6, INPUT_PULLUP);
+  pinMode(PIN_A7, INPUT_PULLUP);
+  pinMode(PIN_A8, INPUT_PULLUP);
+  pinMode(PIN_A9, INPUT_PULLUP);
+  pinMode(PIN_A10, INPUT_PULLUP);
+  pinMode(PIN_A11, INPUT_PULLUP);
+  pinMode(PIN_A12, INPUT);
+  pinMode(PIN_A13, INPUT);
+  pinMode(PIN_A14, INPUT);
+  pinMode(PIN_A15, INPUT);
+
+  // initially all cathodes and anodes off
+  digitalWrite(PIN_CA, HIGH);
+  digitalWrite(PIN_CB, HIGH);
+  digitalWrite(PIN_CC, HIGH);
+  digitalWrite(PIN_CD, HIGH);
+  digitalWrite(PIN_CE, HIGH);
+  digitalWrite(PIN_CF, HIGH);
+  digitalWrite(PIN_CG, HIGH);
+
+  digitalWrite(PIN_AN0, HIGH);
+  digitalWrite(PIN_AN1, HIGH);
+  digitalWrite(PIN_AN2, HIGH);
+  digitalWrite(PIN_AN3, HIGH);
 
 }
 
+// storage for what we read from [A0..A15] inputs
+
+int raw_value = 0;
+int s = 5;
+
+// port C is MSB, port A is LSB
+void read_raw_value() {
+    raw_value = (PINC << 8) | PINA;
+}
+
+void display_value() {
+  PORTB &= ~0x0F;
+  PORTB |= 0x07;  
+  PORTD &= ~0x7F; // clear all digit bits
+  PORTD |= digits[(raw_value & 0xF000) >> 12];
+  delay(s);
+
+  PORTB &= ~0x0F;
+  PORTB |= 0x0B;
+  PORTD &= ~0x7F; // clear all digit bits
+  PORTD |= digits[(raw_value & 0x0F00) >> 8];
+  delay(s);
+
+  PORTB &= ~0x0F;
+  PORTB |= 0x0D;
+  PORTD &= ~0x7F; // clear all digit bits
+  PORTD |= digits[(raw_value & 0x00F0) >> 4];
+  delay(s);
+
+  PORTB &= ~0x0F;
+  PORTB |= 0x0E;
+  PORTD &= ~0x7F; // clear all digit bits
+  PORTD |= digits[(raw_value & 0x000F) ];
+  delay(s );
+
+}
+
+int counter = 0;
 void loop() {
-  digitalWrite(PIN_AN0, LOW);
-  digitalWrite(PIN_C_A, HIGH);
-  delay(500);
-  digitalWrite(PIN_AN0, HIGH);
-  digitalWrite(PIN_C_A, LOW);
-  delay(500);
-  
+  read_raw_value();
+  display_value();
+//  if (counter % 200 == 0) {
+//    raw_value ++;
+//    counter = 0;
+//  }
 }
